7c565ffd237558cf71e2515448b66cc166729fef
tommasovicenzi <tommasovicenzi@studio.unibo.it>
2025-05-08 15:18:26 +0200

    Estetica e nomi

--- fit_sweep_sinusoidale.cpp
+++ fit_sweep_sinusoidale.cpp
@@ -35,9 +35,9 @@ Double_t ris_sin_ind(double *x, double *par) {
   return res;
 }
 
-void fit(TString fname_cond = "V_cond_sin_sweep_1.5Vpp_errori_stand.txt",
-         TString fname_res = "V_res_sweep_sin_1.5Vpp_errori_stand.txt",
-         TString fname_ind = "V_ind_sweep_1.5Vpp_errori_stand.txt",
+void fit(TString fname_cond = "V_cond_sin_sweep_1.5vpp_errori_stand.txt",
+         TString fname_res = "V_res_sweep_sin_1.5vpp_errori_stand.txt",
+         TString fname_ind = "V_ind_sweep_1.5vpp_errori_stand.txt",
          Double_t f0 = 12780, Double_t V0 = 0.05, Double_t R = 600,
          Double_t L = 0.047, Double_t C = 3.3E-9) {
   TFile *file = new TFile("data_cond.root", "RECREATE");
@@ -74,7 +74,7 @@ void fit(TString fname_cond = "V_cond_sin_sweep_1.5Vpp_errori_stand.txt",
   res_res->SetParName(1, "Resistenza");
   res_res->SetParName(2, "Induttanza");
   res_res->SetParName(3, "Capacità");
-  res_res->FixParameter(0,0.75);
+  //res_res->FixParameter(0,0.75);
 
   TGraphErrors *data_cond = new TGraphErrors(fname_cond, "%lg %lg %lg");
   TGraphErrors *data_res = new TGraphErrors(fname_res, "%lg %lg %lg");
@@ -86,9 +86,9 @@ void fit(TString fname_cond = "V_cond_sin_sweep_1.5Vpp_errori_stand.txt",
   data_cond->SetLineColor(4);
   data_cond->SetMarkerColor(4);
   res_cond->Draw("same");
-  data_cond->SetTitle("Figura di diffrazione");
-  data_cond->GetXaxis()->SetTitle("Posizione, m");
-  data_cond->GetYaxis()->SetTitle("Int. luminosa, unit. arb.");
+  data_cond->SetTitle("Condensatore");
+  data_cond->GetXaxis()->SetTitle("Frequenza, Hz");
+  data_cond->GetYaxis()->SetTitle("Ampiezza, V");
   data_cond->GetXaxis()->CenterTitle(true);
   data_cond->GetXaxis()->CenterTitle(true);
   
@@ -97,23 +97,24 @@ void fit(TString fname_cond = "V_cond_sin_sweep_1.5Vpp_errori_stand.txt",
   data_ind->Draw("AP");
   data_ind->SetLineColor(4);
   data_ind->SetMarkerColor(4);
+  data_ind->SetTitle("Induttore");
+  data_ind->GetXaxis()->SetTitle("Frequenza, Hz");
+  data_ind->GetYaxis()->SetTitle("Ampiezza, V");
+  data_ind->GetXaxis()->CenterTitle(true);
+  data_ind->GetXaxis()->CenterTitle(true);
+ 
+ 
   res_ind->Draw("same");
-  data_res->GetXaxis()->SetTitle("frequenza, Hz");
+
+  data_res->SetTitle("Resistore");
+  data_res->GetXaxis()->SetTitle("Frequenza, Hz");
   data_res->GetYaxis()->SetTitle("Ampiezza, V");
-  
   canv->cd(3);
   data_res->Fit("myfunc3", "R");
   data_res->Draw("AP");
   data_res->SetLineColor(4);
   data_res->SetMarkerColor(4);
   res_res->Draw("same");
-  TLegend *leg = new TLegend(.6, .7, .9, .9);
-  leg->SetTextSize(0.04);
-  leg->SetBorderSize(0);  // no border for legend
-  leg->SetFillColor(0);   // fill color is white
-  leg->AddEntry(data_cond, "L= 1.088 m, d=0.101 mm", "p");
-  leg->AddEntry(res_cond, "fit", "l");
-  leg->Draw();
   data_cond->Write();
   file->Close();
   std::cout << "La f di risonanza vale: "
