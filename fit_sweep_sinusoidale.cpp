401984a5ad0bef3b2f23b375368d23149052dc65
NasiLorenzo <146074174+NasiLorenzo@users.noreply.github.com>
2025-05-13 23:03:15 +0200

    boh

--- fit_sweep_sinusoidale.cpp
+++ fit_sweep_sinusoidale.cpp
@@ -42,7 +42,7 @@ void fit(TString fname_cond = "./nuovi_txt/V_cond_1,5_auto.txt",
          Double_t L = 0.047, Double_t C = 3.3E-9) {
   TFile *file = new TFile("data_cond.root", "RECREATE");
   TCanvas *canv = new TCanvas("canv", "Risonanze sinusoidale", 700, 600);
-  canv->Divide(2,2);
+  canv->Divide(2, 2);
   TF1 *res_cond = new TF1("myfunc1", ris_sin_cond, 200, 24000, 4);
   TF1 *res_ind = new TF1("myfunc2", ris_sin_ind, 200, 24000, 4);
   TF1 *res_res = new TF1("myfunc3", ris_sin_res, 200, 24000, 4);
@@ -54,7 +54,7 @@ void fit(TString fname_cond = "./nuovi_txt/V_cond_1,5_auto.txt",
   res_cond->SetParName(1, "Resistenza");
   res_cond->SetParName(2, "Induttanza");
   res_cond->SetParName(3, "Capacità");
-  //res_cond->FixParameter(0,0.75);
+  // res_cond->FixParameter(0,0.75);
 
   res_ind->SetParameter(0, V0);
   res_ind->SetParameter(1, R);
@@ -64,7 +64,7 @@ void fit(TString fname_cond = "./nuovi_txt/V_cond_1,5_auto.txt",
   res_ind->SetParName(1, "Resistenza");
   res_ind->SetParName(2, "Induttanza");
   res_ind->SetParName(3, "Capacità");
-  //res_ind->FixParameter(0,0.75);
+  // res_ind->FixParameter(0,0.75);
 
   res_res->SetParameter(0, V0);
   res_res->SetParameter(1, R);
@@ -74,12 +74,12 @@ void fit(TString fname_cond = "./nuovi_txt/V_cond_1,5_auto.txt",
   res_res->SetParName(1, "Resistenza");
   res_res->SetParName(2, "Induttanza");
   res_res->SetParName(3, "Capacità");
-  //res_res->FixParameter(0,0.75);
+  // res_res->FixParameter(0,0.75);
 
   TGraphErrors *data_cond = new TGraphErrors(fname_cond, "%lg %lg %lg");
   TGraphErrors *data_res = new TGraphErrors(fname_res, "%lg %lg %lg");
   TGraphErrors *data_ind = new TGraphErrors(fname_ind, "%lg %lg %lg");
-  
+
   canv->cd(1);
   data_cond->Fit("myfunc1", "R");
   data_cond->Draw("AP");
@@ -91,7 +91,7 @@ void fit(TString fname_cond = "./nuovi_txt/V_cond_1,5_auto.txt",
   data_cond->GetYaxis()->SetTitle("Ampiezza, V");
   data_cond->GetXaxis()->CenterTitle(true);
   data_cond->GetXaxis()->CenterTitle(true);
-  
+
   canv->cd(2);
   data_ind->Fit("myfunc2", "R");
   data_ind->Draw("AP");
@@ -102,8 +102,7 @@ void fit(TString fname_cond = "./nuovi_txt/V_cond_1,5_auto.txt",
   data_ind->GetYaxis()->SetTitle("Ampiezza, V");
   data_ind->GetXaxis()->CenterTitle(true);
   data_ind->GetXaxis()->CenterTitle(true);
- 
- 
+
   res_ind->Draw("same");
 
   data_res->SetTitle("Resistore");
@@ -118,7 +117,8 @@ void fit(TString fname_cond = "./nuovi_txt/V_cond_1,5_auto.txt",
   data_cond->Write();
   file->Close();
   std::cout << "La f di risonanza vale: "
-            << TMath::Sqrt(1 / (2 * TMath::Pi() * res_cond->GetParameter(2) *
-                                res_cond->GetParameter(3)))
+            << 1. /( 2. * TMath::Pi()) *
+                   TMath::Sqrt(1 / (res_cond->GetParameter(2) *
+                                    res_cond->GetParameter(3)))
             << "\n";
 }
